e44fd27a1624283bf9d658d3cd41b7b798194aaa
JANKI-MODH <156778712+JANKI-MODH@users.noreply.github.com>
2024-03-01 00:01:19 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+// {/* <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script> */}
+
+let btn = document.querySelector("button");
+let url2 = "https://dog.ceo/api/breeds/image/random";
+btn.addEventListener("click", async() => {
+    let link = await getImage();
+    let img = document.querySelector("#result");
+    img.setAttribute("src", link);
+    console.log(link);
+});
+
+async function getImage(){
+    try{
+        let res = await axios.get(url2);
+        return res.data.message;
+    }
+    catch(e){
+        console.log(e);
+        return "NO IMAGES FOUND!!!";
+    }
+}
